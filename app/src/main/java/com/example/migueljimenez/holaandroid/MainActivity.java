91e0461f4d1d664d884c62a24512a98da84e7634
Miguel.Jimenez <miguelbass2009@gmail.com>
2018-03-21 15:04:04 -0700

    Actividad 3ra semana, cambiando propiedades por medio del código fuente

--- app/src/main/java/com/example/migueljimenez/holaandroid/MainActivity.java
+++ app/src/main/java/com/example/migueljimenez/holaandroid/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.migueljimenez.holaandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView myTextView = (TextView)findViewById(R.id.myTextView);
+        myTextView.setText("He modificado TextView con un nuevo texto usando Java");
     }
 }
